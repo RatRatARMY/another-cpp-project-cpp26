543d73628fa1a36ea11b08a2485dc1b90b1d9c5e
RatRatARMY <khang0762412064@outlook.com>
2025-05-09 16:49:02 +0700

    something for that thing again

--- anothermain.cpp
+++ anothermain.cpp
@@ -4,4 +4,10 @@ int main(int argc, char** argv) {
     nghich_ngu::method1();
     std::cout << "Thanh cong roi!" << std::endl;
     std::cout << argv[0] << std::endl;
+    if (argv[argc] == nullptr) {
+        std::cout << "Dung theo chuan" << std::endl;
+    }
+    else if (argv[argc] != nullptr) {
+        std::cout << "Compiler/OS/runtime lib nay bi loan roi (toi nghi vay)" << std::endl;
+    }
 }
--- cppheader/2.cpp
+++ cppheader/2.cpp
@@ -3,7 +3,6 @@
 void nghich_ngu::method1() {
     size_t n = 0;
     while (n < 1'000'000'000) {
-        std::cout << n << std::endl;
         n++;
     }
     std::cout << n << std::endl;
