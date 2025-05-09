a4580c7d056e4cfb909cd887450072c750e31ae3
RatRatARMY <khang0762412064@outlook.com>
2025-05-09 21:26:19 +0700

    another thing for xyzthing

--- cppheader/2.cpp
+++ cppheader/2.cpp
@@ -1,8 +1,10 @@
 #include "2.hpp"
+#include "1.hpp"
 #include <iostream>
+#include <climits>
 void nghich_ngu::method1() {
-    size_t n = 0;
-    while (n < 1'000'000'000) {
+    unsigned long long n = 0ULL;
+    while (n < sys::random_next_ulong(1'000'000'000ULL, ULLONG_MAX)) {
         n++;
     }
     std::cout << n << std::endl;
